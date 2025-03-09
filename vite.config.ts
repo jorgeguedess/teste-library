ece89a420ad727f3cf5f15a8ad54314e1f7893b1
jorgeguedess <jorge_guedes00@hotmail.com>
2025-03-09 20:36:30 -0300

    chore: config deploy lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,47 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { extname, relative, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), libInjectCss(), dts({ include: ["src"] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob
+          .sync("src/**/*.{ts,tsx}", {
+            ignore: ["src/**/*.d.ts"],
+          })
+          .map((file) => [
+            // 1. The name of the entry point
+            // lib/nested/foo.js becomes nested/foo
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
